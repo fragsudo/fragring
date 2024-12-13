68908bef1237ad4a6875f80e018586aaa0315c8d
fragsudo <nyannoyingnyannoyinghewwohewwo@gmail.com>
2024-12-12 22:16:17 -0500

    ok fr done

--- fragweb.js
+++ fragweb.js
@@ -13,11 +13,11 @@ function initWeb() {
                 // clear webId html
                 webId.innerHTML = ``
                 
-                x.foreach(e => {
-                    if (e.url) {
-                        webId.innerHTML += `<a href="${e.url}" target="_blank"><img width="31" height="88" border="0" align="center" src="${e.badgeUrl}" title="${e.name}></a>`
+                x.forEach(e => {
+                    if (e.badgeUrl) {
+                        webId.innerHTML += `<a href="${e.url}" target="_blank"><img width="31" height="88" border="0" align="center" src="${e.badgeUrl}" title="${e.name}"></a>`
                     } else {
-                        webId.innerHTML += `<a href="${e.url}" target="_blank"><img width="31" height="88" border="0" align="center"  src="https://user-images.githubusercontent.com/20684618/31289519-9ebdbe1a-aae6-11e7-8f82-bf794fdd9d1a.png" title="${e.name}></a>`
+                        webId.innerHTML += `<a href="${e.url}" target="_blank"><img width="31" height="88" border="0" align="center"  src="https://user-images.githubusercontent.com/20684618/31289519-9ebdbe1a-aae6-11e7-8f82-bf794fdd9d1a.png" title="${e.name}"></a>`
                     }
                 });
         }).catch(err => console.log(`ERROR ${err}: Error fetching fragweb data.`))
