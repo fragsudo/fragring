90c75f359ab4d949ecac65c1e7deceef265e9d52
fragsudo <nyannoyingnyannoyinghewwohewwo@gmail.com>
2024-12-12 21:34:56 -0500

    fr fr this time

--- fragring.js
+++ fragring.js
@@ -6,29 +6,54 @@ function initRing() {
         let ringId = document.getElementById("fragRingId");
         let currUrl = window.location.href;
 
-        fetch(dat).then(res => res.json()).then(x => {
-            console.log("is able to get into the fetch")
-            // find curr index & name
-            const currIndex = x.findIndex(y => y.url === currUrl);
-            const currName = x[currIndex].name; 
+        fetch(dat)
+            .then(res => {
+                if (!res.ok) { 
+                    throw new Error(`ERROR: failed to fetch fragring data.`);
+                }
+                return res.json();
+            })
+            .then(x => {
+                console.log(res.json)
+                console.log("is able to get into the fetch")
 
-            // populating the ring w/ prev
-            const prevIndex = x[(currIndex - 1 + x.length) % x.length];
-            const prevName = x[prevIndex].name;
-            const prevUrl = x[prevIndex].url;
+                // clear ringId html
+                ringId.innerHTML = ``
 
-            // populating the ring w/ next
-            const nextIndex = x[[(currIndex + 1) % x.length]];
-            const nextName = x[nextIndex].name;
-            const nextUrl = x[nextIndex].url;
+                // find curr index & name
+                let currIndex = x.findIndex(y => y.url === currUrl);
+                // check if curr url is in webring
+                if (currIndex === -1) {
+                    throw new Error(`ERROR: current url ${currUrl} not found in ring data.`);
+                }
+                let currObj = x[currIndex];
+                let currName = currObj.name;
 
-            ringId.innerHTML = `<a href="${prevUrl}>${prevName}</a> FRAGRING: ${currName} <a href="${nextUrl}>${nextName}</a>`
-        })
+                // check if there is prev value, if yes then populate
+                if (typeof x[currIndex-1] !== 'undefined') {
+                    const prevIndex = (currIndex - 1 + x.length) % x.length
+                    const prevObj = x[prevIndex];
+                    const prevName = prevObj.name;
+                    const prevUrl = prevObj.url;
+                    ringId.innerHTML += `<a href="${prevUrl}">${prevName}</a>`;
+                }
+
+                ringId.innerHTML += `<a href="${currUrl}">${currName}</a>`;
+
+                // check if there is next value, if yes then populate
+                if (typeof x[currIndex+1] !== 'undefined') {
+                    const nextIndex = (currIndex + 1) % x.length;
+                    const nextObj = x[nextIndex];
+                    const nextName = nextObj.name;
+                    const nextUrl = nextObj.url;
+                    ringId.innerHTML += `<a href="${nextUrl}">${nextName}</a>`
+                }
+        }).catch(err => console.log(`ERROR ${err}: Error fetching fragring data.`))
     } catch (err) {
-        console.error("ERROR ${err}: Error initializing fragring.");
+        console.error(`ERROR ${err}: Error initializing fragring.`);
     }
 }
 
-window.addEventListener("load", (e) => {
+window.addEventListener("load", () => {
     initRing();
 });
