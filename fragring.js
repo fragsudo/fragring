57141c707c7b38c72e1a16330ebb5120a151b357
fragsudo <nyannoyingnyannoyinghewwohewwo@gmail.com>
2024-12-13 20:11:44 -0500

    change to queryId-based fetching. add fancier html

--- fragring.js
+++ fragring.js
@@ -4,8 +4,8 @@ function initRing() {
     try {
         
         let ringId = document.getElementById("fragRingId");
-        let ringUrl = `https://github.com/fragsudo/fragring`;
-        let currUrl = window.location.href;
+        let queryId = ringId.innerHTML;
+        let ringUrl = `https://fragsudo.github.io/fragring/`;
 
         fetch(dat)
             .then(res => {
@@ -15,42 +15,38 @@ function initRing() {
                 return res.json();
             })
             .then(x => {
-                console.log(`x: ${x}`);
-                console.log("is able to get into the fetch")
+                // console.log(`x: ${x}`);
+                // console.log("is able to get into the fetch")
 
                 // clear ringId html
                 ringId.innerHTML = ``
 
-                // find curr index & name
-                const normalizedCurrUrl = currUrl.replace(/\/+$/, '');
-                const currIndex = x.findIndex(y => y.url.replace(/\/+$/, '') === normalizedCurrUrl);
-                // let currIndex = x.findIndex(y => y.url === currUrl);
+                // find curr obj
+                let normalizedQueryId = queryId.toLowerCase().replace(/\s/g, '');
+                let currIndex = x.findIndex(y => y.queryId === normalizedQueryId);
+                
                 // check if curr url is in webring
                 if (currIndex === -1) {
                     throw new Error(`ERROR: current url ${currUrl} not found in ring data.`);
                 }
+
+                // assign currObj stuff
                 let currObj = x[currIndex];
                 let currName = currObj.name;
 
-                // check if there is prev value, if yes then populate
-                if (typeof x[currIndex-1] !== 'undefined') {
-                    const prevIndex = (currIndex - 1 + x.length) % x.length
-                    const prevObj = x[prevIndex];
-                    const prevName = prevObj.name;
-                    const prevUrl = prevObj.url;
-                    ringId.innerHTML += `<a href="${prevUrl}"><- ${prevName} </a>`;
-                }
+                // check if there is prev value
+                // if no prev value (currIndex is 0, beginning of data), prev is now end of data
+                let prevIndex = (typeof x[currIndex-1] === 'undefined') ? currIndex - 1 + x.length : (currIndex - 1 + x.length) % x.length;
+                let prevObj = x[prevIndex];
+                ringId.innerHTML += `<a href="${prevObj.url}"><- ${prevObj.name} </a>`
+                
+                ringId.innerHTML += `\t\t<a href="${ringUrl}>FRAGRING</a>\t\t`;
 
-                ringId.innerHTML += `<a href="${ringUrl}">FRAGRING (FRAGRANCE RING)</a>`;
-
-                // check if there is next value, if yes then populate
-                if (typeof x[currIndex+1] !== 'undefined') {
-                    const nextIndex = (currIndex + 1) % x.length;
-                    const nextObj = x[nextIndex];
-                    const nextName = nextObj.name;
-                    const nextUrl = nextObj.url;
-                    ringId.innerHTML += `<a href="${nextUrl}">${nextName} -></a>`
-                }
+                // check if there is next value
+                // if no next value (currIndex is end of dat), next is now beginning of data
+                let nextIndex = (typeof x[currIndex-1] === 'undefined') ? (currIndex - 1 + x.length) % x.length : currIndex - 1 + x.length;
+                let nextObj = x[nextIndex];
+                ringId.innerHTML += `<a href="${nextObj.url}">${nextObj.name} -></a>`
         }).catch(err => console.log(`ERROR ${err}: Error fetching fragring data.`))
     } catch (err) {
         console.error(`ERROR ${err}: Error initializing fragring.`);
--- fragweb.js
+++ fragweb.js
@@ -13,13 +13,15 @@ function initWeb() {
                 // clear webId html
                 webId.innerHTML = ``
                 
+                // populate the page with badges
                 x.forEach(e => {
                     if (e.badgeUrl) {
-                        webId.innerHTML += `<a href="${e.url}" target="_blank"><img width="88" height="31" padding="5px" align="center" src="${e.badgeUrl}" title="${e.name}"></a>`
+                        webId.innerHTML += `<a href="${e.url}" target="_blank"><img width="88" height="31" padding="10px" align="center" src="${e.badgeUrl}" title="${e.name}"></a>`
                     } else {
-                        webId.innerHTML += `<a href="${e.url}" target="_blank"><img width="88" height="31" border="0" align="center"  src="https://user-images.githubusercontent.com/20684618/31289519-9ebdbe1a-aae6-11e7-8f82-bf794fdd9d1a.png" title="${e.name}"></a>`
+                        webId.innerHTML += `<a href="${e.url}" target="_blank"><img width="88" height="31" padding="10px" align="center"  src="https://user-images.githubusercontent.com/20684618/31289519-9ebdbe1a-aae6-11e7-8f82-bf794fdd9d1a.png" title="${e.name}"></a>`
                     }
                 });
+                
         }).catch(err => console.log(`ERROR ${err}: Error fetching fragweb data.`))
     } catch (err) {
         console.error(`ERROR ${err}: Error initializing fragweb.`);
