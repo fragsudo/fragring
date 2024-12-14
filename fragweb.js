04c9a72e7a5dbce2c34069b5dd0f14b99146a1ae
fragsudo <nyannoyingnyannoyinghewwohewwo@gmail.com>
2024-12-13 21:32:47 -0500

    add: ability to add names w/ no URLs yet

--- fragweb.js
+++ fragweb.js
@@ -13,13 +13,21 @@ function initWeb() {
                 // clear webId html
                 webId.innerHTML = ``
                 
+                // generic placeholder img
+                let placeholder = "https://user-images.githubusercontent.com/20684618/31289519-9ebdbe1a-aae6-11e7-8f82-bf794fdd9d1a.png";
+
                 // populate the page with badges
                 x.forEach(e => {
-                    if (e.badgeUrl) {
-                        webId.innerHTML += `<a href="${e.url}" target="_blank"><img width="88" height="31" padding="10px" align="center" src="${e.badgeUrl}" title="${e.name}"></a>`
+                    if (e.url) {
+                        if (e.badgeUrl) {
+                            webId.innerHTML += `<a href="${e.url}" target="_blank"><img width="88" height="31" padding="10px" align="center" src="${e.badgeUrl}" title="${e.name}"></a>`;
+                        } else {
+                            webId.innerHTML += `<a href="${e.url}" target="_blank"><img width="88" height="31" padding="10px" align="center"  src="${placeholder}" title="${e.name}"></a>`;
+                        }
                     } else {
-                        webId.innerHTML += `<a href="${e.url}" target="_blank"><img width="88" height="31" padding="10px" align="center"  src="https://user-images.githubusercontent.com/20684618/31289519-9ebdbe1a-aae6-11e7-8f82-bf794fdd9d1a.png" title="${e.name}"></a>`
+                        webId.innerHTML += `<img width="88" height="31" padding="10px" align="center" src="${placeholder}" title="${e.name}">`;
                     }
+                    
                 });
                 
         }).catch(err => console.log(`ERROR ${err}: Error fetching fragweb data.`))
